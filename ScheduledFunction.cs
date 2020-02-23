97e2e26120b76dafcee3a5f73bf732cd386e6390
ronshien <ronshien@gmail.com>
2020-02-23 09:37:06 -0500

    scheduled function

---
+++ ScheduledFunction.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessFuncs
+{
+    public class ScheduledFunction
+    {
+        [FunctionName("ScheduledFunction")]
+        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer,
+            [Table("todos", Connection = "AzureWebJobsStorage")] CloudTable todoTable,
+            ILogger log)
+        {
+            var query = new TableQuery<TodoTableEntity>();
+            var segment = await todoTable.ExecuteQuerySegmentedAsync(query, null);
+            var deleted = 0;
+            foreach (var todo in segment)
+            {
+                if (todo.IsCompleted)
+                {
+                    await todoTable.ExecuteAsync(TableOperation.Delete(todo));
+                    deleted++;
+                }
+            }
+            log.LogInformation($"Deleted {deleted} items at {DateTime.Now}");
+        }
+    }
+}
